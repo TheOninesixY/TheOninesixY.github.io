e3e8cb19763f7db7effd4426959cf6eaa4a0d1cd
OninesixY <theoninesixy@outlook.com>
2026-06-07 18:33:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,14 @@ import { publicFilesPlugin } from './vite-plugin-public-files';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/  ',
     plugins: [react(), tailwindcss(), publicFilesPlugin()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
